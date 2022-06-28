6b011dbad5bfeea29b6e66d4af9cb28c5c40928a
Wessel van Lit <wessel@litdev.nl>
2022-06-28 10:22:27 +0200

    Part 1 - Implemented Entity customization in Editor

--- scripts/entity/Entity.gd
+++ scripts/entity/Entity.gd
@@ -1,3 +1,25 @@
+@tool
 class_name Entity extends Node2D
 
+@export var sprite_index: Vector2i
+@export var fg_color: Color = Color.WHITE
+@export var bg_color: Color = Color.TRANSPARENT
+
+@onready var fg_sprite: Sprite2D = $fg
+@onready var bg_sprite: Sprite2D = $bg
+
 var next_action: Action
+
+func _ready() -> void:
+	_setup()
+
+func _setup() -> void:
+	fg_sprite.modulate = fg_color
+	bg_sprite.modulate = bg_color
+	
+	fg_sprite.frame_coords = sprite_index
+	
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		_setup()
+	
